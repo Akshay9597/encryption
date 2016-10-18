bbd0710349ed9f7e0a1c351d8572043b9ec9aeee
Akshay9597 <abhyankar.akshay@gmail.com>
2016-10-18 20:32:22 +0530

    Done with encryption

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-enc : gcc blowfish.c enc.c main.c -Wall
-dec : gcc blowfish.c dec.c main.c -Wall
-make :
+all : main.o b.o
+	gcc main.o b.o -Wall -o blowfish
+main.o : main.c blowfish.h
+	gcc main.c -c
+b.o : b.c blowfish.h
+	gcc b.c -c
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-/*I want arguments from user as ./enc [KEY] [source file] [target file] or for ./dec........ */
 /*Read 16 bytes and divide into 2 halves*/
+/*Make key strong... Try to type password again if it is not strong .... i.e check for Capitals and symbols*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,32 +9,52 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "blowfish.h"
-//#include "enc.c"
-//#include "dec.c"
 int main(int argc, char *argv[]) {
-/*	unsigned long L =74, R = 10;
-	BLOWFISH_CTX ctx;
-	Blowfish_Init(&ctx, (unsigned char *) "TESTKEY", 7);
-	Blowfish_Encrypt(&ctx, &L, &R);
-	printf("%08lX %08lX\n", L, R);
-	Blowfish_Decrypt(&ctx, &L, &R);
-	printf("%lu %lu\n", L, R);*/
+	/*-----------HELP----------------*/
+	if(argc == 2) {
+		if(!strcmp(argv[1], "-h")) {
+			printf("enc : ./blowfish enc <key> <source file> <target file>\n");
+			printf("dec : ./blowfish dec <key> <source file> <target file>");
+			return 0;
+		}
+	}
+	/*-----------HELP----------------*/
 	unsigned int datalen;
-	int keylen;
+	int keylen, flag = 0;
+	char k;
 	FILE *sfile, *tfile;
 	unsigned int retstrlen;
 	unsigned char key[56], data[17], l[8], r[8];
-	short i;
-	strcpy(key, argv[2]);	
+	short i = -1;
+	if(argc != 5 ) {
+		errno = EINVAL;
+		perror("./blowfish enc/dec <key> <source file> <target file> : ");
+		return errno;
+	}
+	strcpy(key, argv[2]);
+	keylen = strlen(key);
+	/*******Strong key*********/
+	if(!strcmp(argv[1], "enc")) {
+		while(++i < keylen) {
+			if(key[i] < 97 || key[i] > 122) {
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 0) {
+			printf("Key is not strong enough.\nDo you wish to continue y/n\n");
+			k = getchar();
+			if(k == 'n')
+				exit(1);
+/*			if(k == 'y')
+				scanf("%s", key);*/
+		}
+	}
+	/*******Strong key*********/
 	keylen = strlen(key);
 	if(keylen < 4 || keylen > 56) {			/*Argv[1] is my key length 32-448 bits*/
 		perror("Key should be greater than 4 bytes and less than 56 bytes: ");
 		return -1;
-	}
-	if(argc != 5 ) {
-		errno = EINVAL;
-		perror("./a.out enc/dec <key> <source file> <target file>");
-		return errno;
 	}
 	sfile = fopen(argv[3], "r");
 	tfile = fopen(argv[4], "w");
@@ -47,6 +67,18 @@ int main(int argc, char *argv[]) {
 	while(!feof(sfile)) {		//Main loop
 		retstrlen = fread(data, sizeof(char), 16, sfile);		
 		//I have to do padding if datalen != 16... Datalen is != 16 if it reaches at the end.
+		//printf("\n%c\t", data[retstrlen - 1]);
+		if(retstrlen != 16) {
+			for(i = retstrlen - 1; i < 16; i++) {
+				data[i] = ' ';
+			}
+			data[16] = '\n';
+		}
+		//For the last 16 bytes if it reaches end it is taking '\n'... But we don't want.
+		//printf("\n");
+	/*	for(i = 0; i < 16; i++) {
+			printf("%c", data[i]);
+		}*/
 		for(i = 0; i < 8; i++) {		//Dividing into 8 bytes array. 2 halves
 			l[i] = data[i];
 			r[i] = data[i + 8];
@@ -63,5 +95,11 @@ int main(int argc, char *argv[]) {
 		data[16] = '\0';
 		fputs(data, tfile);
 	}
+	fclose(sfile);
+	fclose(tfile);
+	if(!strcmp(argv[1], "enc"))
+		printf("Your data is successfully encrypted in %s\n", argv[4]);
+	else
+		printf("Your data is successfully decrypted in %s\n", argv[4]);
 	return 0;
 }
